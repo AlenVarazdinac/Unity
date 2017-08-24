a7eabb346e25a70b90e574f91ed184f1a2ec09c5
Alen Varazdinac <alen.varazdinac@gmail.com>
2017-08-24 15:05:43 +0200

    Add files via upload

---
+++ ShopScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShopScript : MonoBehaviour 
+{
+	public Image shop;
+	private bool shopOpened = false;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		// Open / Close shop window
+		if (Input.GetKeyDown (KeyCode.E) && shopOpened == false) {
+			shop.enabled = true;
+			shopOpened = true;
+			Debug.Log ("Shop is opened");
+		} 
+		else if (Input.GetKeyDown (KeyCode.E) && shopOpened == true) 
+		{
+			shop.enabled = false;
+			shopOpened = false;
+			Debug.Log ("Shop is closed");
+		}
+
+	}
+}
